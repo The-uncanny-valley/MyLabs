d9a7e061efa58f3ab53a4e1e8901e25ea1858a3e
the-uncanny-valley <runcannyvalley@gmail.com>
2025-06-06 22:57:22 +0300

    add comments

--- .vscode/lab_23/Container.cpp
+++ .vscode/lab_23/Container.cpp
@@ -1,107 +1,107 @@
 // Container.cpp
 #include "Container.h"
-#include <algorithm>
+#include <algorithm> // готовые шаблонные функции
 
-Set::Set() : data(new int[4]), size(0), capacity(4) {}
+Set::Set() : data(new int[4]), size(0), capacity(4) {} // реализация конструктора по умолчанию 
 
 Set::Set(const Set& other) : data(new int[other.capacity]), size(other.size), capacity(other.capacity) {
-    std::copy(other.data, other.data + size, data);
+    std::copy(other.data, other.data + size, data); // вызов функции из библиотеки <algorithm>, которая используется для копирования элементов
+} // конструктор копирования
+
+Set::~Set() { //  метод, который вызывается автоматически, когда объект класса уничтожается
+    delete[] data; //  оператор удаления массива
 }
 
-Set::~Set() {
-    delete[] data;
-}
-
-Set& Set::operator=(const Set& other) {
-    if (this != &other) {
-        delete[] data;
+Set& Set::operator=(const Set& other) { // определение оператора присваивания (=) для класса Set
+    if (this != &other) { // проверка самоприсваивания
+        delete[] data; // освобождаем старую память, которую текущий объект уже держал.
         capacity = other.capacity;
-        size = other.size;
-        data = new int[capacity];
-        std::copy(other.data, other.data + size, data);
+        size = other.size; // копируем информацию о размере
+        data = new int[capacity]; // новый массив под элементы
+        std::copy(other.data, other.data + size, data); // копируем реальные элементы
     }
-    return *this;
+    return *this; // возвращает ссылку на текущий объект
 }
 
-void Set::resize() {
+void Set::resize() { // метод для выделения нового, большего массива
     capacity *= 2;
-    int* newData = new int[capacity];
-    std::copy(data, data + size, newData);
-    delete[] data;
-    data = newData;
+    int* newData = new int[capacity]; // оздаём новый массив на новом участке памяти
+    std::copy(data, data + size, newData); // копируем все существующие элементы в новый массив
+    delete[] data; // удаляем старый массив
+    data = newData; // назначаем новый массив в качестве рабочего
 }
 
-bool Set::contains(int value) const {
+bool Set::contains(int value) const { // реализация метода, который проверяет, есть ли значение в множестве
     for (int i = 0; i < size; ++i)
         if (data[i] == value)
             return true;
     return false;
 }
 
-void Set::add(int value) {
+void Set::add(int value) { // реализация метода Set::add, который добавляет элемент в множество.
     if (!contains(value)) {
-        if (size == capacity)
-            resize();
-        data[size++] = value;
+        if (size == capacity) // если массив уже заполнен
+            resize(); // resize() удваивает capacity
+        data[size++] = value; // добавляем новый элемент в конец массива
     }
 }
 
-int& Set::operator[](int index) {
-    if (index < 0 || index >= size)
-        throw std::out_of_range("Index out of range");
-    return data[index];
+int& Set::operator[](int index) { //  перегрузка оператора индексирования [] для класса Set
+    if (index < 0 || index >= size) // проверка границ
+        throw std::out_of_range("Index out of range"); //  выбрасываем исключение
+    return data[index]; // возвращается реальная ячейка массива, а не её копия
 }
 
-bool Set::operator==(const Set& other) const {
-    if (size != other.size)
+bool Set::operator==(const Set& other) const { // перегрузку оператора == для сравнения двух множеств Set
+    if (size != other.size) // если количество элементов разное, множества точно не равны
         return false;
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < size; ++i) // проверим, что все элементы текущего множества есть в other
         if (!other.contains(data[i]))
             return false;
     return true;
 }
 
-bool Set::operator>(int value) const {
+bool Set::operator>(int value) const { // проверяет, содержит ли множество Set элемент value
     return contains(value);
 }
 
-Set Set::operator-(int n) const {
-    Iterator it(*this);
-    it.moveLeft(n);
+Set Set::operator-(int n) const { // новое множество из элементов текущего, начиная с позиции n (левый "срез")
+    Iterator it(*this); // итератор, привязанный к текущему множеству
+    it.moveLeft(n); // cдвигает итератор влево на n
     Set result;
     for (int i = it.current(); i < size; ++i)
-        result.add(data[i]);
+        result.add(data[i]); // добавляет все элементы с этой позиции до конца в новое множество
     return result;
 }
 
-void Set::print() const {
+void Set::print() const { // метод print, который выводит содержимое множества в консоль
     for (int i = 0; i < size; ++i)
         std::cout << data[i] << " ";
     std::cout << std::endl;
 }
 
-std::ostream& operator<<(std::ostream& os, const Set& set) {
+std::ostream& operator<<(std::ostream& os, const Set& set) { // перегрузка оператора <<
     for (int i = 0; i < set.size; ++i)
         os << set.data[i] << " ";
-    return os;
+    return os; // возвращает поток os, чтобы поддерживать цепочку операций
 }
 
 std::istream& operator>>(std::istream& is, Set& set) {
     int value;
-    is >> value;
-    set.add(value);
-    return is;
+    is >> value; // считывает один элемент из потока
+    set.add(value); // добавляет его в множество
+    return is; // возвращает ссылку на поток
 }
 
-Set::Iterator::Iterator(const Set& s) : set(s), pos(s.size - 1) {}
+Set::Iterator::Iterator(const Set& s) : set(s), pos(s.size - 1) {} // конструктор итератора. Позволяет обходить множество с конца
 
-void Set::Iterator::moveLeft(int n) {
+void Set::Iterator::moveLeft(int n) { // сдвигает итератор влево на n позиций
     pos -= n;
     if (pos < 0) pos = 0;
 }
 
-int Set::Iterator::current() const {
-    if (pos < 0 || pos >= set.size)
+int Set::Iterator::current() const { // возвращает текущую позицию, на которую указывает итератор
+    if (pos < 0 || pos >= set.size) // проверка, указывает ли итератор на допустимый элемент множества
         throw std::out_of_range("Iterator out of range");
     return pos;
 }
